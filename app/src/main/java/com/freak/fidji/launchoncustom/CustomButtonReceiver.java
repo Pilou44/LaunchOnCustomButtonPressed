168a2bcc12e28701d83983199d55d39fb626004e
guillaume <guillaume.beguin@dunasys.com>
2016-04-05 16:58:44 +0200

    [FIX] Fix crash if the wanted activity is not found

--- app/src/main/java/com/freak/fidji/launchoncustom/CustomButtonReceiver.java
+++ app/src/main/java/com/freak/fidji/launchoncustom/CustomButtonReceiver.java
@@ -8,19 +8,25 @@ import android.content.SharedPreferences;
 import android.util.Log;
 
 public class CustomButtonReceiver extends BroadcastReceiver {
+    private static final String TAG = CustomButtonReceiver.class.getSimpleName();
+    private static final boolean DEBUG = true;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("CustomButtonReceiver", "Broadcast received");
+        if (DEBUG)
+            Log.i(TAG, "Broadcast received");
         SharedPreferences preferences = context.getSharedPreferences(MainActivity.PREFERENCE_NAME, 0);
         ComponentName component = null;
         String action = "";
         if (intent.getAction().equals("com.parrot.action.CUSTOM_BUTTON_1")) {
-            Log.i("CustomButtonReceiver", "Button 1");
+            if (DEBUG)
+                Log.i(TAG, "Button 1");
             component = new ComponentName(preferences.getString("package1", "none"), preferences.getString("class1", "none"));
             action = preferences.getString("package1", "none");
         }
         else if (intent.getAction().equals("com.parrot.action.CUSTOM_BUTTON_2")) {
-            Log.i("CustomButtonReceiver", "Button 2");
+            if (DEBUG)
+                Log.i(TAG, "Button 2");
             component = new ComponentName(preferences.getString("package2", "none"), preferences.getString("class2", "none"));
             action = preferences.getString("package2", "none");
         }
@@ -33,7 +39,13 @@ public class CustomButtonReceiver extends BroadcastReceiver {
             newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             newIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
             newIntent.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY);
-            context.startActivity(newIntent);
+            try {
+                context.startActivity(newIntent);
+            }
+            catch (Exception e) {
+                Log.w(TAG, "No activity to launch");
+                e.printStackTrace();
+            }
         }
     }
 }
